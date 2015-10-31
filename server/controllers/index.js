d3ec3ba8193b4a2fdb82f51a5f38515267a3e7db
br-kwon <brian.y.kwon@gmail.com>
2015-10-30 21:17:20 -0700

    Made the client work and now working on server side

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,15 +1,33 @@
 var models = require('../models');
 
 module.exports = {
+
+
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    get: function (req, res) {
+      //create a signal that are model can hear
+      res.send('GET request to homepage');
+      models['messages'].get();
+
+
+    }, // a function which handles a get request for all messages
+    post: function (req, res) {
+      res.send('POST request to homepage');
+      models['messages'].post();
+      //create a signal that model can hear
+    } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {}
-  }
+    get: function (req, res) {
+      res.send('GET request to homepage');
+      models['users'].get();
+    },
+    post: function (req, res) {
+      res.send('POST request to homepage');
+      models['users'].post();
+    }
+  },
 };
 
